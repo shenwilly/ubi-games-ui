9d2903ac042464562b4202039c50483684b7cb3c
willy <shen.willy95@gmail.com>
2021-07-03 14:35:20 +0700

    add walletconnect

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,1 +1,2 @@
 export const APP_VERSION = process.env.REACT_APP_VERSION || '0.1.0'
+export const INFURA_ID = process.env.REACT_APP_INFURA_ID!
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export * from "./app";
+export * from "./networks";
+export * from "./urls";
