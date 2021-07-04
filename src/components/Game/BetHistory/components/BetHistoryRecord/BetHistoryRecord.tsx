a1fc621506e3368e51d1e166a3ae4288b1673d4c
willy <shen.willy95@gmail.com>
2021-07-04 11:46:49 +0700

    BetHistoryRecord ui

--- src/components/Game/BetHistory/components/BetHistoryRecord/BetHistoryRecord.tsx
+++ src/components/Game/BetHistory/components/BetHistoryRecord/BetHistoryRecord.tsx
@@ -1,22 +1,42 @@
 import { Box, Text, Flex, Spacer } from "@chakra-ui/react";
+import { BigNumber } from "ethers";
 import { Bet } from "../../../../../types";
+import { formatBN } from "../../../../../utils/bigNumber";
+import { format, fromUnixTime } from 'date-fns'
+import { useMemo } from "react";
 
 interface BetHistoryRecordProps {
   bet: Bet
 }
 
 const BetHistoryRecord: React.FC<BetHistoryRecordProps> = ({ bet }) => {
+  const { amount, chance, prize, timestamp, finished, win } = bet;
+  const betAmount = useMemo(() => formatBN(BigNumber.from(amount)), [amount]);
+  const prizeAmount = useMemo(() => formatBN(BigNumber.from(prize), 4), [prize]);
+  const date = useMemo(() => fromUnixTime(parseInt(timestamp)), [timestamp]);
+  const dateLabel = useMemo(() => format(date, 'Pp'), [date]);
+
   return (
     <Box borderRadius={8} p={3} bg="white" border="1px" borderColor="gray.300" width="100%">
       <Flex>
-        <Text>0.1 UBI @ 50%</Text>
+        <Text>Bet: {betAmount} UBI</Text>
         <Spacer/>
-        <Text fontWeight="bold">Pending</Text>
+        <Text fontWeight="bold">
+          {finished 
+            ? win
+              ? 'WIN'
+              : 'LOSE'
+            : 'PENDING'
+          }
+        </Text>
+      </Flex>
+      <Flex>
+        <Text>Chance: {chance}%</Text>
       </Flex>
       <Flex alignItems="flex-end">
-        <Text>Payout: 0.1 UBI</Text>
+        <Text>Payout: {prizeAmount} UBI</Text>
         <Spacer/>
-        <Text fontSize="sm">{bet.timestamp}</Text>
+        <Text fontSize="sm">{dateLabel}</Text>
       </Flex>
     </Box>
   );
