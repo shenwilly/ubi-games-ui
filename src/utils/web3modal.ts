ed5e89081a2a99addb5c3782fd6b4926fb4c4694
willy <shen.willy95@gmail.com>
2021-07-05 18:51:26 +0700

    disable walletconnect

--- src/utils/web3modal.ts
+++ src/utils/web3modal.ts
@@ -1,15 +1,19 @@
-import WalletConnectProvider from "@walletconnect/web3-provider";
+// import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
-import { INFURA_ID } from "../constants"
+// import { INFURA_ID } from "../constants"
 
 export const web3Modal = new Web3Modal({
+    // network: "matic",
     cacheProvider: true,
-    providerOptions: {
-        walletconnect: {
-            package: WalletConnectProvider,
-            options: {
-                infuraId: INFURA_ID,
-            },
-        },
-    },
+    // providerOptions: {
+    //     walletconnect: {
+    //         package: WalletConnectProvider,
+    //         options: {
+    //             infuraId: INFURA_ID,
+    //             rpc: {
+    //                 137: "https://rpc-mainnet.maticvigil.com",
+    //             },
+    //         },
+    //     },
+    // },
 });
